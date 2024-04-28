e79da3047c7516b6402e1136d324bcb37a02f943
knoff <selby@niko.ink>
2024-04-28 10:26:03 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "wasm-app";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.cargo;
+          rustc = pkgs.rustc;
+        };
+        commonBuildInputs = with pkgs; [
+          openssl.dev
+          pkg-config
+          zlib.dev
+        ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+          libiconv
+          CoreServices
+          SystemConfiguration
+        ]);
+      in
+      rec {
+        packages.actix-web-example = rustPlatform.buildRustPackage rec {
+          pname = "app";
+          version = "0.1.0";
+          src = ./.;
+
+
+          nativeBuildInputs = with pkgs; [
+            (rust-bin.stable."1.76.0".default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            pkg-config
+            trunk
+            binaryen
+            dart-sass
+            tailwindcss
+            wasm-bindgen-cli
+          ];
+
+          buildInputs = commonBuildInputs;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          buildPhase = ''
+            runHook preBuild
+            export XDG_CACHE_HOME="$TMPDIR/cache"
+            mkdir -p $XDG_CACHE_HOME
+            mkdir -p $TMPDIR/output
+            trunk build --release --offline --dist $TMPDIR/output
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            cp -r $TMPDIR/output/* $out/bin/
+            runHook postInstall
+          '';
+
+        };
+
+        defaultPackage = packages.actix-web-example;
+
+        devShell = pkgs.mkShell {
+          inherit commonBuildInputs;
+          nativeBuildInputs = with pkgs; [
+            cargo-edit
+            cargo-generate
+            (rust-bin.stable."1.76.0".default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            rust-analyzer
+            sccache
+            pkg-config
+            trunk
+            tailwindcss
+            binaryen
+            dart-sass
+            wasm-bindgen-cli
+          ];
+          RUST_BACKTRACE = 1;
+        };
+      }
+    );
+}
+
